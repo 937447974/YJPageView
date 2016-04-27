63b797d5b100ebb1d5854f3dd549d563ad346667
阳君 <937447974@qq.com>
2016-04-27 19:34:33 +0800

    类创建

---
+++ Classes/YJPageView.h
@@ -0,0 +1,13 @@
+//
+//  YJPageView.h
+//  YJPageView
+//
+//  Created by admin on 16/4/27.
+//  Copyright © 2016年 YJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YJPageView : UIView
+
+@end
---
+++ Classes/YJPageView.m
@@ -0,0 +1,21 @@
+//
+//  YJPageView.m
+//  YJPageView
+//
+//  Created by admin on 16/4/27.
+//  Copyright © 2016年 YJ. All rights reserved.
+//
+
+#import "YJPageView.h"
+
+@implementation YJPageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
